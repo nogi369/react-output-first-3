536ec398378da3a37e03a8551a0ff374c7d08960
nogi <Createc369.Re@gmail.com>
2023-08-27 02:23:25 +0900

    add: 削除機能

--- src/components/organisms/TodoList/index.jsx
+++ src/components/organisms/TodoList/index.jsx
@@ -2,14 +2,17 @@ import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const TodoList = (props) => {
-  const { todoList } = props;
+  const { todoList, handleDeleteTodo } = props;
 
   return (
     <ul>
       {todoList.map((todo) => (
         <li key={todo.id}>
           <span>{todo.title}</span>
-          <FontAwesomeIcon icon={faTrashAlt} size="lg" />
+          <FontAwesomeIcon
+          icon={faTrashAlt} size="lg"
+          onClick={() => {handleDeleteTodo(todo.id, todo.title)}}
+          />
         </li>
       ))}
     </ul>
--- src/components/templates/TodoTemplate/index.jsx
+++ src/components/templates/TodoTemplate/index.jsx
@@ -5,10 +5,19 @@ import { TodoList } from "../../organisms/TodoList";
 export const TodoTemplate = () => {
   const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
 
+  const handleDeleteTodo = (targetId, targetTitle) => {
+    // 「OK」時の処理開始 ＋ 確認ダイアログの表示
+    if (window.confirm(`「${targetTitle}」のtodoを削除しますか？`)) {
+      const newTodoList = originTodoList.filter((todo) => todo.id !== targetId)
+
+      setOriginTodoList(newTodoList);
+    }
+  }
+
   return (
     <>
       <h1>Todo List</h1>
-      <TodoList todoList={originTodoList} />
+      <TodoList todoList={originTodoList} handleDeleteTodo={handleDeleteTodo} />
     </>
   );
 };
