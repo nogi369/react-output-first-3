bda8271f13af944523c0635db770db9b03c22c80
nogi <Createc369.Re@gmail.com>
2023-08-28 20:02:14 +0900

    add: テスト環境構築

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/react-output-first/",
+  test: {
+    globals: true,
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.ts'
+ },
 })
