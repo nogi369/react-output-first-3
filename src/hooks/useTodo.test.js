318ad5f6db5a6f271e5ff4ab7a928b87017aa119
nogi <Createc369.Re@gmail.com>
2023-08-30 19:25:02 +0900

    add: 【関数テスト】handleChangeSearchKeyword

--- src/hooks/useTodo.test.js
+++ src/hooks/useTodo.test.js
@@ -135,138 +135,31 @@ describe("【hooksテスト】useApp test", () => {
       expect(result.current[0].showTodoList).toEqual(expectTodoList);
     });
   });
+  describe("【関数テスト】handleChangeSearchKeyword", () => {
+    test("【正常系】検索ワードがある場合、検索された結果が反映される", () => {
+      // 予測値(期待値)
+      const expectValue = [INIT_TODO_LIST[0]]; // Listが被り、ややこしくならないように「 expectValue 」と変数名にしている
+      // 引数
+      const eventObject = {
+        target: {
+          value: "Todo1",
+        },
+      };
+      const { result } = renderHook(() => useTodo());
+      act(() => result.current[1].handleChangeSearchKeyword(eventObject));
+      expect(result.current[0].showTodoList).toEqual(expectValue);
+    });
+  });
+  test("【正常系】検索ワードがない場合、元のTodoリストが反映される", () => {
+    const expectValue = INIT_TODO_LIST;
+    // 引数
+    const eventObject = {
+      target: {
+        value: "",
+      },
+    };
+    const { result } = renderHook(() => useTodo());
+    act(() => result.current[1].handleChangeSearchKeyword(eventObject));
+    expect(result.current[0].showTodoList).toEqual(expectValue);
+  });
 });
-
-/**
- * ＜テスト流れ＞
- * データ用意: 予測値(期待値) = 配列、引数 = オブジェクト
- * 引数を初期化
- * 用意したデータを編集する
- * カスタムフック呼び出し
- * テスト実行
- */
-
-/**
- * ＜ TC: 削除 ＞
- * (自解)
- * targetId, targetTitleが渡ってきたとき、Todoが削除されること
- * confirmモーダルで「 OK 」を押した場合、Todoが削除されること
- * todoListが更新されること
- * (サンプル)
- * todoが削除されること
- * confirmでキャンセルをクリックした場合、todoが削除されないこと
- * "検索キーワードがある場合" TODO: 確認: 検索結果でshowTodoListが更新されること (showTodoListへもTodo削除処理の結果が反映されること)
- */
-
-// 最初にデータを用意する
-// 文字入力を受け付ける(◯)
-// 発火
-// 新規データ作成
-// データ作成に伴う各種更新処理
-
-/**
- * ＜テストケース＞
- *
- * (自分)
- * 新規データを作成できること
- * 配列に新規データを追加できること
- * データ作成に伴う各種更新ができること
- *
- * (サンプル)
- * todoList, uniqueIdが更新されること、addInputValueがリセットされること(正)
- * エンターキーを押していない場合、処理が発生しないこと(異)
- * 入力値がない場合、処理が発生しないこと(異)
- * "検索キーワードがある場合" TODO: 確認1: 検索結果でshowTodoListが更新されること
- *
- * ＜テストケース掘り下げ＞
- *
- * todoList, uniqueIdが更新されること、addInputValueがリセットされること( TC: 1 )
- *
- * (自分の思考)
- * => todoList = 配列, uniqueId = id を用意する必要がありそう・・・
- *
- * (サンプル)
- * データ用意: 予測値(期待値) = 配列、引数 = オブジェクト
- * 引数を初期化
- * 用意したデータを編集する
- * カスタムフック呼び出し
- * テスト実行
- *
- *
- * エンターキーを押していない場合、処理が発生しないこと( TC: 2 )
- *
- * (自分の思考)
- * 新規データ定義(必要なもの)
- * eventObject.key = ""(期待する結果)
- * エンターキーを押していない場合の定義が分からない
- *
- * (サンプル)
- * フォームに入力したTodoタイトルで更新されること
- * 表示用TodoListが予想通り更新されないこと
- * 入力値(addInputValue)がリセットされない
- *
- *
- * 入力値がない場合、処理が発生しないこと( TC: 3 )
- * (自分の思考)
- * 「入力値がない場合」の定義どうすればいいのだろうか？
- *
- *
- * (サンプル)
- * eventObject.target.value = ""; //入力値が空
- * eventObject.key = ""; // 「Enterキーが押されない」ことも入力値に含む
- * expect(result.current[0].addInputValue).toBe(""); // onChangeAddInputValueのテストで確かめてるので、handleAddTodoのテストでは記述不要
- *
- *
- * 削除TC
- *
- * (自分の思考)
- * Todoが削除できること
- * confirmモーダルでキャンセルしたとき、Todoが削除できないこと
- *
- * (サンプル)
- * todoが削除されること
- * confirmでキャンセルをクリックした場合、todoが削除されないこと
- *
- * (感じたこと)
- * サンプルのTCの方が、より具体的なものとなっている
- *
- *
- * todoが削除されること( TC: 1 )
- * (自分の思考)
- * 配列を用意
- * わからないこと
- * hooks関数に引数として渡す値は(eventObject? clickイベント？)
- * Todoを削除するのに、入力値は要らないはず
- *
- * (サンプル)
- * 用意ブロック
- * 予測値(配列)を用意
- * 予測値(配列)の初期化
- *
- * testブロック
- * targetId
- * targetTitle
- * window.confirmをモック化(mock: https://qiita.com/Fudeko/items/301f8a80963dfcaafb80#mock%E3%81%A8%E3%81%AF)
- * expectTodoListに削除対象のidをフィルターしたINIT_TODO_LISTを代入
- * hooks呼び出し
- * テスト実行
- * handleDeleteTodoに渡すのは、テストブロックで定義した(targetId, targetTitle)
- * 「表示用TodoListが予想通り更新されないこと」を確認する(テストで確認すること)
- *
- *
- * confirmでキャンセルをクリックした場合、todoが削除されないこと( TC: 2 )
- * (自分の思考)
- * 操作の観点から、"【正常系】todoが削除されること"の逆のコードを定義すればいいと思っていた
- * => 配列の確認はfilterしていないnewTodoListとshowTodoListを突き合わせて一致しないことを確かめればいいと思っていた
- *
- * (サンプル)
- * 比較用の配列:expectTodoListには、通常の配列: INIT_TODO_LISTを指定すれば良かった(filterは不要)
- *
- * (感じたこと)
- * 基本的に使わなくて済むのなら、「 .not.toEqual 」は使わない
- *
- *
- *
- * (自分の思考)
- * (サンプル)
- */
